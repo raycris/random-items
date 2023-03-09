f6a58aa04b3c4c0ce51d002b9fedb46657cc1369
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-03-09 15:56:36 -0400

    usando useRef

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,10 +1,13 @@
+import { useRef } from "react";
 import Image from "next/image";
 
 type imageProps = { image: string };
 
 export function RandomFox({ image }: imageProps): JSX.Element {
+  const node = useRef<HTMLImageElement>(null);
   return (
     <Image
+      ref={node}
       width={320}
       height={300}
       src={image}
