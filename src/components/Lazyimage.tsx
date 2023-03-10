cb164cd4876c9d1ce4d1125f92715ef275f55359
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-03-10 16:06:11 -0400

    Challenge: unobserve when loaded

--- src/components/Lazyimage.tsx
+++ src/components/Lazyimage.tsx
@@ -16,33 +16,37 @@ export function Lazyimage({
   ...imgProps
 }: Props): JSX.Element {
   const node = useRef<HTMLImageElement>(null);
+  const [isLazyLoaded, setIsLazyLoaded] = useState(false);
   const [currentSrc, setCurrentSrc] = useState(
     "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
   );
 
   // Para hacer el Lazy loading
   useEffect(() => {
+    if (isLazyLoaded) {
+      return;
+    }
     // Nuevo observador
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          setCurrentSrc(src);
-
-          if (typeof onLazyLoad === "function") {
-            onLazyLoad(node.current);
-          }
+        if (!entry.isIntersecting || !node.current) {
+          return;
         }
       });
     });
 
-    if (node.current) {
-      observer.observe(node.current);
+    setCurrentSrc(src);
+    observer.disconnect();
+    setIsLazyLoaded(true);
+
+    if (typeof onLazyLoad === "function") {
+      onLazyLoad(node.current);
     }
 
     return () => {
       observer.disconnect();
     };
-  }, [onLazyLoad, src]);
+  }, [isLazyLoaded, onLazyLoad, src]);
 
   return <Image ref={node} src={currentSrc} alt="Fox" {...imgProps} />;
 }
